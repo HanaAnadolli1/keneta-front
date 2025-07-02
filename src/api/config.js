beba5a57ed1a8d4bd972b1c2cd2aee04ede415b9
HanaAnadolli1 <hana.anadolli@progtelx.com>
2025-07-02 12:42:56 +0200

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,5 @@
-const API_ROOT = import.meta.env.VITE_API_ROOT;
+export const API_ROOT = import.meta.env.VITE_API_ROOT || "";
 
+// v1 and cart endpoints
 export const API_V1 = `${API_ROOT}/api/v1`;
 export const API_CART = `${API_ROOT}/api`;
