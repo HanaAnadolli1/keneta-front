8ed77a33681fad5d115aaa8945c9dbf21b5033f7
HanaAnadolli1 <hana.anadolli@progtelx.com>
2025-07-07 14:35:16 +0200

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -5,5 +5,5 @@ export const API_ROOT = DEV
   ? "" // Vite proxy handles API in dev
   : "/api"; // Vercel rewrites /api to real API in prod
 
-export const API_V1 = `${API_ROOT}/api/v1`;
-export const API_CART = `${API_ROOT}/api`;
+export const API_V1 = `${API_ROOT}/v1`;
+export const API_CART = `${API_ROOT}`;
